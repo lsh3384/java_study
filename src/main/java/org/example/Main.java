2cdc4410e005656f35642975a845ff214b1616f6
lee <lsh3384@gmail.com>
2022-11-30 21:24:09 +0900

    studying time, date, and formatting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        ForIteration forIteration = new ForIteration();
-        forIteration.printDiagonal();
+        DateTimeFormatting dateTimeFormatting = new DateTimeFormatting();
+        dateTimeFormatting.dateFormatting();
     }
 }
