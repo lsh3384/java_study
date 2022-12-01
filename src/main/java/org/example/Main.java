47a4010399cede822289353aa706099114a70542
lee <lsh3384@gmail.com>
2022-12-01 21:47:00 +0900

    collections framework arraylist study

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        DateTimeFormatting dateTimeFormatting = new DateTimeFormatting();
-        dateTimeFormatting.dateFormatting();
+        CollectionsFramework collectionsFramework = new CollectionsFramework();
+        collectionsFramework.arrayListEx1();
     }
 }
