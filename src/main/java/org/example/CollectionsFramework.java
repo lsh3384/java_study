ccdd1c93e8abc31be1c744e85962941a6807b1d4
lee <lsh3384@gmail.com>
2022-12-03 20:37:23 +0900

    collections framework vector study

--- src/main/java/org/example/CollectionsFramework.java
+++ src/main/java/org/example/CollectionsFramework.java
@@ -1,8 +1,10 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.Vector;
 
 public class CollectionsFramework {
     public void arrayListEx1() {
@@ -72,4 +74,35 @@ public class CollectionsFramework {
             System.out.println(list.get(i));
         }
     }
+
+    public void vectorEx1() {
+        Vector v = new Vector(5);
+        v.add("1");
+        v.add("2");
+        v.add("3");
+        print(v);
+
+        v.trimToSize();
+        System.out.println("=== After trimToSize ===");
+        print(v);
+
+        v.ensureCapacity(6);
+        System.out.println("=== After ensureCapacity(6) ===");
+        print(v);
+
+        v.setSize(7);
+        System.out.println("=== After setSize(7) ===");
+        print(v);
+
+        v.clear();
+        System.out.println("=== After clear() ===");
+        print(v);
+    }
+
+    public static void print(Vector v) {
+        System.out.println(v);
+        System.out.println("size: " + v.size());
+        System.out.println("capacity: " + v.capacity());
+    }
 }
+
