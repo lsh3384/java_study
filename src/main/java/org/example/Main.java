f75d5034a3e9f9a38df7e7aff4be7a3713fab5c9
lee <lsh3384@gmail.com>
2022-12-07 22:19:15 +0900

    expValidCheck

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         StackAndQueue stackAndQueue = new StackAndQueue();
-        stackAndQueue.staticStackEx1();
+        stackAndQueue.expValidCheck("((2+3)*1)+3");
     }
 }
--- src/main/java/org/example/StackAndQueue.java
+++ src/main/java/org/example/StackAndQueue.java
@@ -53,4 +53,29 @@ public class StackAndQueue {
         if(!back.empty())
             forward.push(back.pop());
     }
+
+    public void expValidCheck(String expression) {
+        Stack st = new Stack();
+        System.out.println("expression:"+ expression);
+
+        try {
+            for(int i=0; i < expression.length();i++) {
+                char ch = expression.charAt(i);
+
+                if(ch =='(') {
+                    st.push(ch+"");
+                } else if (ch == ')') {
+                    st.pop();
+                }
+            }
+
+            if(st.isEmpty()) {
+                System.out.println("괄호가 일치합니다.");
+            } else {
+                System.out.println("괄호가 일치하지 않습니다.");
+            }
+        } catch (EmptyStackException e) {
+            System.out.println("괄호가 일치하지 않습니다.");
+        }
+    }
 }
