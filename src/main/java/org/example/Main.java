7e53ef3685b6b4b0a375c0a5c6ebf3fa693d1169
lee <lsh3384@gmail.com>
2022-12-08 22:04:38 +0900

    queue ex1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         StackAndQueue stackAndQueue = new StackAndQueue();
-        stackAndQueue.expValidCheck("((2+3)*1)+3");
+        stackAndQueue.queueEx1();
     }
 }
