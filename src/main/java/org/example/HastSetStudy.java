04efdda85bf98c7954139891740cb8737cd3d0be
lee <lsh3384@gmail.com>
2022-12-19 21:48:53 +0900

    hash set study hashLotto

--- src/main/java/org/example/HastSetStudy.java
+++ src/main/java/org/example/HastSetStudy.java
@@ -13,4 +13,17 @@ public class HastSetStudy {
 
         System.out.println(set);
     }
+
+    public void hashLotto () {
+        Set set = new HashSet();
+
+        for (int i = 0; set.size() < 6; i++) {
+            int num = (int) (Math.random() * 45) + 1;
+            set.add(new Integer(num));
+        }
+
+        List list = new LinkedList(set);
+        Collections.sort(list);
+        System.out.println(list);
+    }
 }
