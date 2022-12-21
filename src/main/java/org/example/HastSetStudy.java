d7f08fa6d19bb297d4f5492df58270bc65674cd7
lee <lsh3384@gmail.com>
2022-12-21 23:33:17 +0900

    hash set study bingo

--- src/main/java/org/example/HastSetStudy.java
+++ src/main/java/org/example/HastSetStudy.java
@@ -26,4 +26,23 @@ public class HastSetStudy {
         Collections.sort(list);
         System.out.println(list);
     }
+
+    public void bingo() {
+        Set set = new HashSet();
+        int[][] board = new int[5][5];
+
+        for(int i=0; set.size() < 25; i++) {
+            set.add((int)(Math.random() * 50) +1+"");
+        }
+
+        Iterator it = set.iterator();
+
+        for(int i=0; i < board.length; i++) {
+            for(int j=0; j < board[i].length; j++) {
+                board[i][j] = Integer.parseInt((String)it.next());
+                System.out.print((board[i][j] < 10 ? "  " : " ") + board[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
