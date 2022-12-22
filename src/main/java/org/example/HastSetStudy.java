5c9f5aa397f9fb91d299bfff9536a04fc0a628b1
lee <lsh3384@gmail.com>
2022-12-22 21:33:33 +0900

    hash set example 3

--- src/main/java/org/example/HastSetStudy.java
+++ src/main/java/org/example/HastSetStudy.java
@@ -45,4 +45,30 @@ public class HastSetStudy {
             System.out.println();
         }
     }
+
+    public void hashSetEx3() {
+        HashSet set = new HashSet();
+
+        set.add("abc");
+        set.add("abc");
+        set.add(new Person("David", 10));
+        set.add(new Person("David", 10));
+
+        System.out.println(set);
+    }
+
+}
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + ":" + age;
+    }
 }
