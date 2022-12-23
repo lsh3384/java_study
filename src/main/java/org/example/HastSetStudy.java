35a460311fb8dda936601c3192377af739cee319
lee <lsh3384@gmail.com>
2022-12-23 11:45:05 +0900

    hash set exmaple 4

--- src/main/java/org/example/HastSetStudy.java
+++ src/main/java/org/example/HastSetStudy.java
@@ -49,6 +49,17 @@ public class HastSetStudy {
     public void hashSetEx3() {
         HashSet set = new HashSet();
 
+        set.add(new String("abc"));
+        set.add(new String("abc"));
+        set.add(new Person("David", 10));
+        set.add(new Person("David", 10));
+
+        System.out.println(set);
+    }
+
+    public void hashSetEx4() {
+        HashSet set = new HashSet();
+
         set.add("abc");
         set.add("abc");
         set.add(new Person("David", 10));
@@ -68,6 +79,33 @@ class Person {
         this.age = age;
     }
 
+    public String toString() {
+        return name + ":" + age;
+    }
+}
+
+
+class Person2 {
+    String name;
+    int age;
+
+    Person2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean equals(Object obj) {
+        if(obj instanceof Person2) {
+            Person2 tmp = (Person2)obj;
+            return name.equals(tmp.name) && age == tmp.age;
+        }
+
+        return false;
+    }
+
+    public int hashCode() {
+        return (name+age).hashCode();
+    }
     public String toString() {
         return name + ":" + age;
     }
